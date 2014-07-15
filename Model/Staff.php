e13b309bbec7a67323eab0c41ffe005c5ed98df2
Kasun Kodagoda <kvkrusl@gmail.com>
2014-07-15 22:33:30 +0530

    [add:php] Add Models to the project
    Add Staff and Student models to the Models folder.

---
+++ Model/Staff.php
@@ -0,0 +1,33 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Staff Model
+ *
+ */
+class Staff extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'first_name';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'id' => array(
+			'blank' => array(
+				'rule' => array('blank'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+}
---
+++ Model/Student.php
@@ -0,0 +1,88 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Student Model
+ *
+ * @property FieldGroup $FieldGroup
+ * @property StudentProgress $StudentProgress
+ * @property TaskAssigner $TaskAssigner
+ */
+class Student extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'first_name';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'id' => array(
+			'blank' => array(
+				'rule' => array('blank'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'FieldGroup' => array(
+			'className' => 'FieldGroup',
+			'foreignKey' => 'field_group_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'StudentProgress' => array(
+			'className' => 'StudentProgress',
+			'foreignKey' => 'student_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		),
+		'TaskAssigner' => array(
+			'className' => 'TaskAssigner',
+			'foreignKey' => 'student_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+}
