bc72e0661c5be6b0d22592523ccda7bb254410f5
kaushalkdn <kaushalnihathamana@gmail.com>
2014-07-19 18:05:17 +0530

    [add:php] Add Model to the project

---
+++ Model/WHR.php
@@ -0,0 +1,46 @@
+<?php
+App::uses('AppModel', 'Model');
+
+class WHR extends AppModel {
+
+	public $validate = array(
+		'id' => array(
+			'blank' => array(
+				'rule' => array('blank'),
+				'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'date' => array(
+			'date' => array(
+				'rule' => array('date'),
+				'message' => 'Must be a valid date.',
+			),
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				'message' => 'Date must not be empty.',
+			),
+		),
+		'value' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				'message' => 'WHR must not be empty.',
+			),
+			'decimal' => array(
+				'rule' => array('decimal', 2),
+				'message' => 'WHR must be a decilmal value with 2 decimal points.',
+			),
+		),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+	public $belongsTo = array(
+		'FamilyMember' => array(
+			'className' => 'FamilyMember',
+			'foreignKey' => 'family_member_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
