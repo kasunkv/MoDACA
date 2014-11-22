0b004a9c52dd73a54d6695970872f2ccccc567f7
Kasun Kodagoda <kvkrusl@gmail.com>
2014-11-22 14:58:12 +0530

    [add:api] Add the api folder conating the project for the API
    Add the project containing the code to create the API to access the
    database using the mobile application.

---
+++ api/Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'port' => '3306',
+		'host' => 'localhost',
+		'login' => 'kasunkv',
+		'password' => 'kvk123',
+		'database' => 'modaca_db',
+	);
+}
